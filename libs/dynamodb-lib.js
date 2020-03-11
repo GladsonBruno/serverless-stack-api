96ae3bdea435baccbfbd1501b535a66e90eb2641
GladsonBruno <gladsonbruno16@gmail.com>
2020-03-11 16:05:31 -0300

    Possivel correção do bug de acesso

--- libs/dynamodb-lib.js
+++ libs/dynamodb-lib.js
@@ -1,8 +1,6 @@
 import AWS from "aws-sdk";
 
 export function call(action, params) {
-  // Update region to America do Sul (São Paulo)
-  AWS.config.update({ region: "sa-east-1" });
   const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
   return dynamoDb[action](params).promise();
